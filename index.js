19ff3402948478b06471e1e1b6f7fdc568be5067
Yasaman Esmaeili <yasamanesmaeili@Yasamans-MacBook-Air.local>
2023-04-27 19:07:53 -0400

    feat:new updates added

---
+++ index.js
@@ -0,0 +1,31 @@
+// Create the initial customer object
+const customer = {
+  firstName: "jake",
+  lastName: "smith",
+  email: "jaekSmih!aol.com",
+  phone: undefined,
+  zipCode: "631",
+  favoriteFlavors: 32,
+  cupSize: "medium",
+  favoriteStore: "Target",
+  firstVisit: false
+};
+
+// Modify the incorrect information
+customer["email"] = "Jak3Smith1992@email.com";
+customer["phone"] = 3195551234;
+customer["zipCode"] = "63132";
+customer["favoriteFlavors"] = ["coffee", "strawberry", "matcha"];
+
+// Remove unwanted properties
+delete customer.zipCode;
+delete customer.favoriteStore;
+
+// Add new properties
+customer.toppings = ["chocolate sprinkles", "wafer straws", "gummy bears"];
+customer.futureFlavors = "mango";
+customer.todaysPurchaseCost = 5.32;
+
+// Print the keys in the survey
+const keys = Object.keys(customer);
+console.log(keys);
